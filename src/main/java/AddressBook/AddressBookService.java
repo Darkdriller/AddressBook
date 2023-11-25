96b3c8306e79b3f29feb1c35f2c3e5255a82884e
Exypnos <swaindhruv28@gmail.com>
2023-11-25 15:14:19 +0530

    Refactored: Main, AddressBookService Added:Updating Contact for UC3

--- src/main/java/AddressBook/AddressBookService.java
+++ src/main/java/AddressBook/AddressBookService.java
@@ -3,9 +3,17 @@ package AddressBook;
 import java.util.Map;
 
 public class AddressBookService {
-    // Making First Name as key and other contact details as value
+    // Making First Name and last name as key and other contact details as value
     Map<String, AddressBookDetails> dtoMap = DataBase.dtoMap;
     public void addData(AddressBookDetails dto){
-        DataBase.dtoMap.put(dto.getFirstName(), dto);
+        DataBase.dtoMap.put(dto.getFirstName() + dto.getLastName() , dto);
+    }
+
+    public void updateAddressBook(AddressBookDetails addressBookDeets) {
+        Map<String, AddressBookDetails> dtoMap = DataBase.dtoMap;
+        if(dtoMap.containsKey(addressBookDeets.getFirstName())){
+            DataBase.dtoMap.put(addressBookDeets.getFirstName() + addressBookDeets.getLastName(), addressBookDeets);
+        } else
+            addData(addressBookDeets);
     }
 }
--- src/main/java/AddressBook/Main.java
+++ src/main/java/AddressBook/Main.java
@@ -2,7 +2,10 @@ package AddressBook;
 import java.util.Scanner;
 
 public class Main {
-
+    public static AddressBookService addressBookService = new AddressBookService();
+    private static void updateRecords() {
+        addressBookService.updateAddressBook(UserInput.userInputFromConsole());
+    }
     // Main Method
     public static void main(String[] args) {
         System.out.println("**********Welcome to Address Book System Service*********");
@@ -15,13 +18,29 @@ public class Main {
         Print.printDetails(DataBase.dtoMap);
 
         String input = "Start";
+
         while(!input.equalsIgnoreCase("quit")) {
-            // PrintUtils.print(DataBase.dtoMap);
-            addressBookService.addData(UserInput.userInputFromConsole());
-            Print.printDetails(DataBase.dtoMap);
+            System.out.println("enter quit to Quit or press any Button to Continue");
             input = sc.nextLine();
+            System.out.println("Pick up the correct choice!");
+            System.out.println("1. Add New Contact");
+            System.out.println("2. Update Existing Contact");
+            int choice = sc.nextInt();
+            switch(choice){
+                case 1:
+                    addressBookService.addData(UserInput.userInputFromConsole());
+                    break;
+                case 2:
+                    updateRecords();
+                    break;
+                default:
+                    System.out.println("Please Enter valid choice!!");
+                    break;
+            }
+            Print.printDetails(DataBase.dtoMap);
+
         }
+    }
 
 
     }
-}
